80fee73095c84b6bd11228f9b5114230e9ae3345
KintsugiCode <liam.going@code.berlin>
2021-03-24 11:38:02 +0100

    [graphql] CreateIngredient mutation added

--- ingredient/mutations.py
+++ ingredient/mutations.py
@@ -4,14 +4,18 @@ from .models import Ingredient
 from .type import IngredientType
 
 
-class IngredientInput(graphene.InputObjectType):
+class IngredientTagInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+
+class CreateIngredientInput(graphene.InputObjectType):
     name = graphene.String()
     description = graphene.String()
-
+    # image =
+    tags = graphene.List(IngredientTagInput, required=True)
 
 class CreateIngredient(graphene.Mutation):
     class Arguments:
-        data = IngredientInput(required=True)
+        data = CreateIngredientInput(required=True)
 
     Output = IngredientType
 
