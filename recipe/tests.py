1f8836ff7c5a700120dbf5954571b33d73fe44c8
KintsugiCode <liam.going@code.berlin>
2021-04-23 16:20:24 +0200

    [tests] tests added for responses of "allApp" queries

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from graphene_django.utils.testing import GraphQLTestCase
+
+from recipe.models import Recipe
+
+
+class RecipeTestCase(GraphQLTestCase):
+    def test_all_recipes_query(self):
+        Recipe.objects.create(
+            name='TestRecipe12345',
+            duration='30'
+            )
+        response = self.query(
+            '''query allRecipes {
+        allRecipes {
+            id
+            name
+        }
+        }
+        ''',
+            op_name="allRecipes",
+        )
+        response = response.json()['data']
+        self.assertEqual(len(response['allRecipes']), 1)
+        self.assertEqual(response['allRecipes'][0]['name'], 'TestRecipe12345')
--- tag/tests.py
+++ tag/tests.py
@@ -2,7 +2,7 @@ import json
 
 from graphene_django.utils.testing import GraphQLTestCase
 
-from tag.models import Tag
+from tag.models import Tag, Category
 
 
 class TagTestCase(GraphQLTestCase):
@@ -21,3 +21,20 @@ class TagTestCase(GraphQLTestCase):
         response = response.json()['data']
         self.assertEqual(len(response['allTags']), 1)
         self.assertEqual(response['allTags'][0]['name'], 'TestTag12345')
+
+class CategoryTestCase(GraphQLTestCase):
+    def test_all_categories_query(self):
+        Category.objects.create(name='TestCategory12345')
+        response = self.query(
+            '''query allCategories {
+        allCategories {
+            id
+            name
+        }
+        }
+        ''',
+            op_name="allCategories",
+        )
+        response = response.json()['data']
+        self.assertEqual(len(response['allCategories']), 1)
+        self.assertEqual(response['allCategories'][0]['name'], 'TestCategory12345')
