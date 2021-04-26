4c8a0e20ae9778ecb97b3db9755e25dfaf9b1d90
aarnav <aarnavbos@gmail.com>
2021-04-26 14:09:18 +0000

    [chores] Added STATIC_ROOT

--- greenit/settings.py
+++ greenit/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 '''
 
+import os
 from pathlib import Path
 
 from decouple import config
@@ -156,3 +157,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
