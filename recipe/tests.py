c4c6ca9c77a876f78a6ab519c00a655f0d6ad676
KintsugiCode <liam.going@code.berlin>
2021-05-11 11:48:10 +0200

    [test] recipe unit tests made atomic

--- recipe/tests.py
+++ recipe/tests.py
@@ -26,52 +26,62 @@ class RecipeCreateTest(TestCase):
     def tearDown(self):
         self.recipe.delete()
 
-    def test_correct(self):
+    def test_recipe_creation(self):
         self.assertTrue(
             self.recipe is not None,
             msg='Recipe creation failed',
         )
+    def test_recipe_name(self):
         self.assertEqual(self.recipe.name, 'TestRecipe', 'Recipe name creation failed')
+    def test_recipe_description(self):
         self.assertEqual(
             self.recipe.description,
             'A test recipe.',
             'Recipe description creation failed',
         )
+    def test_recipe_video_url(self):
         self.assertEqual(
             self.recipe.video_url,
             'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
             'Recipe video_url creation failed',
         )
+    def test_recipe_rating(self):
         self.assertEqual(
             self.recipe.rating,
             4,
             'Recipe rating creation failed',
         )
+    def test_recipe_duration(self):
         self.assertEqual(
             self.recipe.duration,
             30,
             'Recipe duration creation failed',
         )
+    def test_recipe_author(self):
         self.assertEqual(
             self.recipe.author.name,
             'TestUser',
             'Recipe author creation failed',
         )
+    def test_recipe_category(self):
         self.assertEqual(
             self.recipe.category.name,
             'TestCategory',
             'Recipe category creation failed',
         )
+    def test_recipe_expiry(self):
         self.assertEqual(
             self.recipe.expiry,
             'TestExpiry',
             'Recipe expiry creation failed',
         )
+    def test_recipe_author_notes(self):
         self.assertEqual(
             self.recipe.notes_from_author,
             'TestNotes',
             'Recipe notes_from_author creation failed',
         )
+    def test_recipe_is_featured(self):
         self.assertEqual(
             self.recipe.is_featured,
             True,
@@ -81,7 +91,7 @@ class RecipeCreateTest(TestCase):
 
 
 
-class RecipeQueryTest(GraphQLTestCase):
+class AllRecipeQueryTest(GraphQLTestCase):
     def test_all_recipes_query(self):
         Recipe.objects.create(name='TestRecipe', duration='30')
         response = self.query(
