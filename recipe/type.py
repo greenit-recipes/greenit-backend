b7b53134cd581cec863c2fc4a9c41bd785948c73
aarnav <aarnavbos@gmail.com>
2021-06-01 13:06:16 +0100

    [feature] Add filter field on recipe queries [change] Remove Utensil and Ingredient filter on Recipe [change] Refactor Tag filter on Recipe

--- recipe/type.py
+++ recipe/type.py
@@ -1,7 +1,6 @@
 import graphene
 from graphene.types.generic import GenericScalar
 from graphene_django import DjangoObjectType
-
 from ingredient.type import IngredientAmountType
 
 from .models import Recipe
@@ -18,7 +17,9 @@ class RecipeType(DjangoObjectType):
     def resolve_ingredients(parent, info):
         return parent.ingredients.through.objects.filter(recipe__id=parent.id)
 
-    ingredients = graphene.List(graphene.NonNull(IngredientAmountType), required=True, default_value=[])
+    ingredients = graphene.List(
+        graphene.NonNull(IngredientAmountType), required=True, default_value=[]
+    )
 
     class Meta:
         model = Recipe
