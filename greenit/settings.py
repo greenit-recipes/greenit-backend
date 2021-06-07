6cc95d5240ecb96e0fddba13320ccee5626c7c2f
KintsugiCode <liam.going@code.berlin>
2021-06-07 19:57:59 +0200

    [chores] Allowed Hosts updated

--- greenit/settings.py
+++ greenit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['134.122.74.71', 'localhost', '127.0.0.1', 'www.greenitcommunity.com']
+ALLOWED_HOSTS = ['134.122.74.71', 'localhost', '127.0.0.1', 'api.greenitcommunity.com']
 
 
 # Application definition
