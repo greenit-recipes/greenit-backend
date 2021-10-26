160dd6e7bf707b5ee012ec8453aca335ec3439d9
Florian <florian@MacBook-Pro-de-Florian.local>
2021-10-27 01:45:41 +0200

    change gitignore and delete secu for graphql playground

--- greenit/urls.py
+++ greenit/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=False))),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:
