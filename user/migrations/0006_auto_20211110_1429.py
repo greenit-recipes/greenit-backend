2fcfe89518ce29ef2b6c6db08d9bf95ffd853e44
Florian <florian@MacBook-Pro-de-Florian.local>
2021-12-07 11:30:16 +0100

    change model

--- user/migrations/0006_auto_20211110_1429.py
+++ user/migrations/0006_auto_20211110_1429.py
@@ -51,7 +51,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='username',
-            field=models.CharField(default='SOME STRING', max_length=140, unique=True),
+            field=models.CharField(max_length=140, unique=True),
         ),
         migrations.AlterField(
             model_name='user',
