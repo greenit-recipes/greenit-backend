ef44d6c9ae31151e92a80b33b4507b20e83a9a67
Florian <florian@MacBook-Pro-de-Florian.local>
2021-12-08 19:24:12 +0100

    update script

--- beta-deploy.sh
+++ beta-deploy.sh
@@ -5,10 +5,10 @@ git checkout develop
 git pull
 yarn install
 npm run build
-scp -i /Users/florian/.ssh/greenit-aws-beta.pem /Users/florian/Desktop/greenit-webapp/build ubuntu@13.38.18.186:/var/www/greenit-backend/react
+scp -ri /Users/florian/.ssh/greenit-aws-beta.pem /Users/florian/Desktop/greenit-webapp/build ubuntu@13.38.18.186:/var/www/greenit-backend/react
 
 echo "transfert .env"
-scp -i /Users/florian/.ssh/greenit-aws-beta.pem .env.beta ubuntu@13.38.18.186:/var/www/greenit-backend
+scp -i /Users/florian/.ssh/greenit-aws-beta.pem /Users/florian/Desktop/greenit-backend/.env.beta ubuntu@13.38.18.186:/var/www/greenit-backend
 echo "---------- Run docker deploy ----------"
 ssh -ti /Users/florian/.ssh/greenit-aws-beta.pem ubuntu@13.38.18.186 "pwd && cd /var/www/greenit-backend && ./docker-deploy-beta.sh"
 echo "---------- END SCRIPT BETA DEPLOY ----------"
