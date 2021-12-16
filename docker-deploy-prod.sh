75553090e299a485257669cd3a3e3715674c59a0
Florian <florian@MacBook-Pro-de-Florian.local>
2021-12-16 17:19:48 +0100

    create scipt for deploiement

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -3,17 +3,21 @@
 
 git fetch
 git checkout develop
-# If that's not the case, we pull the latest changes and we build a new image
 
 echo -e "Start pull"
 git pull origin develop;
-
+if [ -z "$(git status --porcelain)" ]; then 
+    echo "Le dossier est clean (back)"
+else 
+    echo "Il reste des fichiers pas commit"
+    exit 0
+fi
 # Docker
 echo -e "Run docker"
 
 docker stop core_web
 docker stop core_app
-docker-compose -f docker-compose.beta.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build
 until [ "`docker inspect -f {{.State.Running}} core_app`"=="true" ]; do
     sleep 0.1;
 done;
--- prod-deploy.sh
+++ prod-deploy.sh
@@ -5,7 +5,33 @@ set -e
 echo "Tu va mettre en production c'est good pour toi ? (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo "pd"
+    cd /Users/florian/Desktop/greenit-webapp
+    git checkout develop
+    git pull
+    if [ -z "$(git status -uno --porcelain)" ]; then 
+        echo "Le dossier est clean (front)"
+    else 
+        echo "Il reste des fichiers pas commit"
+        exit 0
+    fi
+    reslog=$(git log HEAD..origin/develop --oneline)
+    if [[ "${reslog}" != "" ]] ; then
+        echo "Il reste des fichiers pas push ou pull (front)"
+        exit 0
+    fi
+    yarn install
+    npm run build:prod
+    scp -ri /Users/florian/.ssh/aws_prod.pem /Users/florian/Desktop/greenit-webapp/build ubuntu@15.188.47.157:/var/www/greenit-backend/react
+
+    if [ $1 ]
+    then
+        exit 0
+    fi
+    echo "transfert .env"
+    scp -i /Users/florian/.ssh/aws_prod.pem /Users/florian/Desktop/greenit-backend/.env.prod ubuntu@15.188.47.157:/var/www/greenit-backend
+    echo "---------- Run docker deploy ----------"
+    ssh -ti /Users/florian/.ssh/aws_prod.pem ubuntu@15.188.47.157 "pwd && cd /var/www/greenit-backend && ./docker-deploy-prod.sh"
+    echo "---------- END SCRIPT PROD DEPLOY ----------"
 else
-    echo "pas bon"
+    echo "Exit"
 fi
