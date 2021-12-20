d3087ad7e5b031ac4a63f522335a547c5d14725a
Florian <florian@MacBook-Pro-de-Florian.local>
2021-12-20 11:12:29 +0100

    add comment admin

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from comment.models import CommentRecipe
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (
+        'id',
+        'comment',
+        'author_id',
+        'recipe_id',
+        'created_at',
+    )
+    search_fields = ['id', 'comment', 'author__id', 'author__username']
+    pass
+
+
+admin.site.register(CommentRecipe, CommentAdmin)
