2749e66f663f1362b686859a0a9c6e0c615dda49
Florian <florian@MacBook-Pro-de-Florian.local>
2021-12-21 23:59:20 +0100

    update for deploy

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -11,7 +11,7 @@ echo -e "Run docker"
 
 docker stop core_web
 docker stop core_app
-docker-compose -f docker-compose.prod.yml up -d --build
+sudo docker-compose -f docker-compose.prod.yml up -d --build
 until [ "`docker inspect -f {{.State.Running}} core_app`"=="true" ]; do
     sleep 0.1;
 done;
