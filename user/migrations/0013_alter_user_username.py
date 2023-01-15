9ab09174df1cea9da52491e4e42b3a2790107fff
Sos-Adrien <adrien.sosson@code.berlin>
2023-01-15 21:01:17 +0100

    fix trial with Zack 15/01

--- user/migrations/0013_alter_user_username.py
+++ user/migrations/0013_alter_user_username.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0012_user_graphql_auth_admin'),
+        ('user', '0011_user_is_follow_newsletter'),
     ]
 
     operations = [
